291ba70ad4dc448c7eed03ad94b95f02a30c6502
Maksym Yarema <macsym2@gmail.com>
2023-11-15 10:44:36 +0200

    calculating integral with default java multithreading tools implementing RunnableCalculator

--- src/main/java/yaremax/Main.java
+++ src/main/java/yaremax/Main.java
@@ -1,6 +1,10 @@
 package yaremax;
 
 public class Main {
+
+    private double totalResult;
+    private int finishedThreads;
+
     public static void main(String[] args) {
         Main main = new Main();
         main.run();
@@ -18,5 +22,41 @@ public class Main {
 
         System.out.println("res = " + res);
         System.out.println("time = " + (finishTime-startTime) + " ms");
+    }
+
+    public void run2() {
+        double a = 0;
+        double b = Math.PI;
+        int n = 1_000_000_000;
+        int nThreads = 10;
+        totalResult = 0;
+        finishedThreads = 0;
+        double delta = (b - a) / nThreads;
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < nThreads; i++) {
+            double ai = a + i * delta;
+            double bi = ai + delta;
+            new Thread(new RunnableCalculator(ai, bi, n/nThreads, Math::sin, this)).start();
+        }
+
+        try {
+            synchronized (this) {
+                while (finishedThreads < nThreads) {
+                    wait();
+                }
+            }
+            long finishTime = System.currentTimeMillis();
+            System.out.println("res = " + totalResult);
+            System.out.println("time = " + (finishTime-startTime) + " ms");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void sendResult(double result) {
+        totalResult += result;
+        finishedThreads++;
+        notify();
     }
 }
---
+++ src/main/java/yaremax/RunnableCalculator.java
@@ -0,0 +1,19 @@
+package yaremax;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class RunnableCalculator implements Runnable {
+
+    private final IntegralCalculator integralCalculator;
+    private final Main main;
+
+    public RunnableCalculator(double a, double b, int n, DoubleUnaryOperator func, Main main) {
+        integralCalculator = new IntegralCalculator(a, b, n, func);
+        this.main = main;
+    }
+
+    @Override
+    public void run() {
+        main.sendResult(integralCalculator.calculate());
+    }
+}
